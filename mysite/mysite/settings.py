2ee9cf8ebb10a6e9f0a6ef598e611db3eede2313
Inger Klekacz <ingernet@gmail.com>
2020-04-04 13:42:30 -0700

    updating settings with mysql database stuff

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polls',
+        'USER': secrets['db_user'],
+        'PASSWORD': secrets['db_password'],
+        'HOST': secrets['db_host'],
+        'PORT': secrets['db_port'],
     }
 }
 
